26caeb69acc1230d9f64aab640557c1796c22913
Ayushman Chabri <chabriayushman@gmail.com>
2026-02-12 19:01:38 +0530

    Update 6

---
+++ loaders/nutrient_provision_loader.py
@@ -0,0 +1,13 @@
+import loaders.base_loader
+from base_loader import load_json
+
+def load_nutrient_provision_data(region):
+    '''
+    Docstring for load_nutrient_provision_data
+    
+    Method to call nutrient provision data
+    
+
+    '''
+    return load_json(f"nutrient_provision/{region.lower()}_nutrient_provision.json")
+    
---
+++ loaders/prices_loader.py
@@ -0,0 +1,8 @@
+import loaders.base_loader
+from base_loader import load_json
+
+def load_prices_data(region):
+    '''
+    Method to load prices
+    '''
+    return load_json(f"prices/{region.lower()}_prices.json")
